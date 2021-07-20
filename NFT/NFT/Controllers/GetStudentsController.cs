f162496d02f75877fdf0487a9b59426eb68942ed
Nevinia <nevinia210@gmail.com>
2021-07-21 00:07:01 +0200

    first try

---
+++ NFT/NFT/Controllers/GetStudentsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NFT.Controllers
+{
+    public class GetStudentsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult CreateStudent()
+        {
+            return View("CreateStudent", null);
+        }
+
+        [HttpPost]
+        public ActionResult CreateStudent(IOModel input)
+        {
+            input.OutputString = input.InputString + "9";
+            return View("CreateStudent9", input);
+        }
+    }
+}
---
+++ NFT/NFT/Models/IOModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NFT.Models
+{
+    public class IOModel
+    {
+        public string InputString { get; set; }
+        public string OutputString { get; set; }
+    }
+}
